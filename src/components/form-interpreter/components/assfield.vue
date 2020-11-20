f0f85269229730b93a336ce4df54394ee48332be
666 <3313751430@qq.com>
2020-11-20 16:03:55 +0800

    update

--- src/components/form-interpreter/components/assfield.vue
+++ src/components/form-interpreter/components/assfield.vue
@@ -18,12 +18,16 @@ export default {
     return {
       showVal: "",
     };
-  },
+  }, 
   created() {
     this.$on("asstable-selected", ({ cpnt, data }) => {
       if (cpnt.fid === this.cpnt.data._selectedTable) {
         if (data.length) {
-          this.cpnt.data._val = data[0][this.cpnt.data._selectedField];
+          let value = data[0][this.cpnt.data._selectedField];
+          if (_.isNull(value) || typeof value === "undefined") {
+            value = "";
+          }
+          this.cpnt.data._val = value;
           const showVal = data[0][this.cpnt.data._selectedField + "_x"];
           if (typeof showVal !== "undefined") {
             this.showVal = this.cpnt.data.__showVal__ = showVal;
